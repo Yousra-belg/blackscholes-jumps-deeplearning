3eb7bc379afeacc1752c775ba5b8c0c52c2ec7aa
Yousra-belg <belgyousra@gmail.com>
2026-04-03 23:56:04 +0100

    Create params.py

---
+++ params.py
@@ -0,0 +1,6 @@
+r     = 0.05   # taux sans risque
+sigma = 0.20   # volatilité
+alpha = 0.10   # amplitude du saut
+lam   = 0.50   # intensité risque-neutre
+K     = 100.0  # strike
+T     = 1.0    # maturité
